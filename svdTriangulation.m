baa86492aa54f540c1f41bfbc913705fa1b2485d
Andre Wagner <wagner@briggs.math.tu-berlin.de>
2017-08-21 10:14:30 +0200

    started restructuring for more convenient and general usability

--- svdTriangulation.m
+++ svdTriangulation.m
@@ -1,4 +1,4 @@
-function [kernel, B ] = svdTriangulation(A,x)
+function [kernel, r ] = svdTriangulation(A,x)
 [~,worldDim]=size(A{1});
 Asigma=cat(1,A{:});
 x=num2cell(x',2);
@@ -6,7 +6,7 @@ xEmbed=blkdiag(x{:})';
 B=[Asigma xEmbed];
 r=worldDim-rank(Asigma);
 [u,d,v]=svd(B);
-d(end-r:end,end-r:end)=0;
+d(:,end-r:end)=0;
 kernel=null(u*d*v');
 end
 
